3034222dd8f3c3876c3e9a59a001bf1427ea7845
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-19 16:04:17 -0400

    Practice rest and spread operators

---
+++ script.js
@@ -0,0 +1,13 @@
+//Rest Operator
+function sum(...args) {
+    return args.reduce((a, b) => a + b);
+  };
+console.log(sum(1.5, 2.0, 3.3, 4.1, 5.9));
+
+//Spread Operator
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  arr2 = [...arr1];
+})();
+console.log(arr2);
