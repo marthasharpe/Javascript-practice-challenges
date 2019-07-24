607351ccd4b7b18746e3aad133e2447595619c63
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-24 11:44:36 -0400

    Match whitespace and non-whitespace

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function sum(...args) {
     return args.reduce((a, b) => a + b);
   };
-console.log(sum(1.5, 2.0, 3.3, 4.1, 5.9));
+console.log(sum(1.5, 2.0, 3.3, 4.1, 5.9));//16.8
 
 //Spread Operator
 const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
@@ -19,7 +19,7 @@ const scotchSoda = {
   Mixer: 'Soda',
 }
 const {Glass : x, Spirit : y, Mixer : z} = scotchSoda;
-console.log(y);
+console.log(y);//'Scotch'
 
 //Destructuring Assignment to Assign Variables from Nested Objects
 const LOCAL_FORECAST = {
@@ -30,7 +30,7 @@ function getMaxOfTmrw(forecast) {
   const { tomorrow : { max : maxOfTomorrow}} = forecast;
   return maxOfTomorrow;
 }
-console.log(getMaxOfTmrw(LOCAL_FORECAST));
+console.log(getMaxOfTmrw(LOCAL_FORECAST));//84.6
 
 //Destructuring with Rest Operator
 const source = [1,2,3,4,5,6,7,8,9,10];
@@ -64,7 +64,7 @@ class Vegetable {
   }
 }
 const carrot = new Vegetable('carrot');
-console.log(carrot.name);
+console.log(carrot.name);//'carrot'
 
 //Getters and Setters
 let user = {
@@ -86,92 +86,97 @@ console.log(user.surname);
 //.test()
 let myString = "Hello, World!";
 let stringRegex = /Hello/;
-let result1 = stringRegex.test(myString);
-console.log(result1);
+console.log(stringRegex.test(myString));//true
 
 //.match()
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
-let result2 = extractStr.match(codingRegex);
-console.log(result2);
+console.log(extractStr.match(codingRegex));//'coding'
 
 // -i insensitive flag ignores cases, -g global flag gets all instances
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /twinkle/ig;
-let result3 = twinkleStar.match(starRegex);
-console.log(result3);
+console.log(twinkleStar.match(starRegex).length);//2
 
 // . matches any character
 let exampleStr = "Let's have a tun of fun with regular expressions!";
 let unRegex = /.un/g;
-let result4 = unRegex.test(exampleStr);
-console.log(result4);
+console.log(unRegex.test(exampleStr));//true
 
 // square brackets match character sets
 let bugQuote = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/ig;//will find all vowels
-let result5 = bugQuote.match(vowelRegex);
-console.log(result5);
+console.log(bugQuote.match(vowelRegex).length);//25
 
 // a hyphen matches all characters or numbers in a range
 let userName = "MarthaSharpe2020";
 let rangeRegex = /[a-z0-9]/ig;
-let result6 = userName.match(rangeRegex);
-console.log(result6);
+console.log(rangeRegex.test(userName));//true
 
 // a caret designates the characters NOT to match
 let miceQuote = "3 blind mice.";
 let caretRegex = /[^aeiou0-9]/ig;
-let result7 = bugQuote.match(caretRegex);
-console.log(result7);
+console.log(miceQuote.match(caretRegex));
 
 // + finds characters that occur one or more times
 let difficultSpelling = "Mississippi Sisters";
 let manyRegex = /s+/gi;
-let result8 = difficultSpelling.match(manyRegex);
-console.log(result8);
+console.log(difficultSpelling.match(manyRegex).length);//5
 
 // * finds characers that occur zero or more times
 let soccerWord = "gooooooooal!";
 let gPhrase = "gut feeling";
 let oPhrase = "over the moon";
 let goRegex = /go*/;
-console.log(soccerWord.match(goRegex));
-console.log(gPhrase.match(goRegex));
-console.log(oPhrase.match(goRegex));
+console.log(soccerWord.match(goRegex));//"goooooooo"
+console.log(gPhrase.match(goRegex));//"g"
+console.log(oPhrase.match(goRegex));//null
 
 //greedy vs. lazy quantifiers
 let greedy = /".+"/g;
 let lazy = /".+?"/g;
 let str = 'a "witch" and her "broom" is one';
-console.log(str.match(greedy));
-console.log(str.match(lazy));
+console.log(str.match(greedy));//"witch" and her "broom"
+console.log(str.match(lazy));//"witch" "broom"
 
 // caret searches for a match at the beginning of strings
 // dollar sign searches at the end of strings
 let train = "engine or caboose";
 let start = /^engine/;
 let end = /caboose$/;
-console.log(start.test(train));
-console.log(end.test(train));
+console.log(start.test(train));//true
+console.log(end.test(train));//true
 
 // \w is short for [A-Za-z0-9_], all alphanumeric characters
 let userSearch = "Figaro_1990";
 let alphabetRegexV2 = /\w/g;
-let result9 = userSearch.match(alphabetRegexV2).length;
-console.log(result9);
+console.log(userSearch.match(alphabetRegexV2).length);//11
 
 // \W is short for [^A-Za-z0-9_], all non-alphanumeric characters
 let shortHand = /\W/;
 let numbers = "42%";
 let sentence = "Coding!";
-console.log(numbers.match(shortHand));
-console.log(sentence.match(shortHand));
+console.log(numbers.match(shortHand));//%
+console.log(sentence.match(shortHand));//!
 
 // \d is short for [0-9], a single digit
 // \D is short for [^0-9], a single non-digit
 let numString = "The Magnificent 7";
 let numRegex = /\d/g;
 let noNumRegex = /\D/g;
-console.log(numString.match(numRegex).length);
-console.log(numString.match(noNumRegex).length);
+console.log(numString.match(numRegex).length);//16
+console.log(numString.match(noNumRegex).length);//1
+
+// curly braces indicate number of repetitions allowed: e.g. {2,4} no fewer than 2 no more than 4
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]{2,}\d*$/i;
+//case-insensitive, letters at the beginning, no fewer than 2, zero or more numbers at the end
+console.log(userCheck.test(username));//true
+
+// \s matches white space including \r (return), \t (tab), \f (form-feed), \n (new-line), \v (vertical tab)
+// \S matches non white space or [^\r\t\f\n\v]
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g;
+let countNonWhiteSpace = /\S/g;
+console.log(sample.match(countWhiteSpace).length);//5
+console.log(sample.match(countNonWhiteSpace).length);//38
