ed16d11c6960d67f827044fb21516541a3a7508e
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-08 14:20:42 -0400

    Convert from binary

--- script.js
+++ script.js
@@ -981,5 +981,14 @@
 //   arr.forEach(flatten);
 //   return flatArray;
 // }
-// console.log(steamrollArray([1, [2], [3, [[4]]]]));
+// console.log(steamrollArray([1, [2], [3, [[4]]]]));//[1, 2, 3, 4]
 
+function binaryAgent(str) {
+  let binaryArray = str.split(' ');
+  let wordArray = [];
+  binaryArray.forEach(element => {
+     wordArray.push(String.fromCharCode(parseInt(element, 2)));
+  });
+  return wordArray.join('');
+}
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
