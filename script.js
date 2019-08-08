b4a4c47bf7881c658f4777bad20544ad6f01c597
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-08 15:53:39 -0400

    Check truthy using Boolean()

--- script.js
+++ script.js
@@ -994,9 +994,10 @@
 // }
 // console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
 
-//check if all objects have a given property
+//check if all objects have truthy values using Boolean function
 function truthCheck(collection, pre) {
-  
-  return pre;
+  return collection.every(object => object.hasOwnProperty(pre) && Boolean(object[pre]));
 }
-console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": null}], "sex"));
+
+
