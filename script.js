3023a0b8481205138dfb542474cccdb87e351033
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-09 16:01:45 -0400

    Roman Numeral Converter

--- script.js
+++ script.js
@@ -1090,8 +1090,13 @@
 //Roman Numeral Converter
 function convertToRoman(num) {
   let romanNum = "";
-  const romanNumerals = { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1, }
-  
-  return romanNum; 
+  const romanNumerals = { 'M':1000, 'CM':900, 'D':500, 'CD':400, 'C':100, 'XC':90, 'L':50, 'XL':40, 'X':10, 'IX':9, 'V':5, 'IV':4, 'I':1 }
+  for (let i in romanNumerals){
+    while (num >= romanNumerals[i]) {
+      romanNum += i;
+      num -= romanNumerals[i];
+    }
+  }
+  return romanNum;
 }
- console.log(convertToRoman(21));
+ console.log(convertToRoman(891));
