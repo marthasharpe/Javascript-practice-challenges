4d64cd221d74be615b4bfe9e61693bf50fec2e4a
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-10 13:08:27 -0400

    Cash Register setup

--- script.js
+++ script.js
@@ -1118,10 +1118,20 @@
 // }
 // console.log(rot13("SERR PBQR PNZC!"));
 
-//Telephone Number Validator
-function telephoneCheck(str) {
-  let regex =  /^(1[\-\s]?)?(\(\d{3}\)|\d{3})[\-\s]?\d{3}[\-\s]?\d{4}$/;
-  //return str.match(regex);
-  return regex.test(str);
+// //Telephone Number Validator
+// function telephoneCheck(str) {
+//   let regex =  /^(1[\-\s]?)?(\(\d{3}\)|\d{3})[\-\s]?\d{3}[\-\s]?\d{4}$/;
+//   //return str.match(regex);
+//   return regex.test(str);
+// }
+// console.log(telephoneCheck("15555555555"));
+
+//Cash Register
+//cid === cash-in-drawer array with available change
+function checkCashRegister(price, cash, cid) {
+  let status = "";
+  let change = [];
+  // Here is your change, ma'am.
+  return { status, change };
 }
-console.log(telephoneCheck("15555555555"));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
