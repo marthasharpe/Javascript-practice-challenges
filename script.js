1898b66ca36a8167efbdfe715f5db579672b6047
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-10 13:00:29 -0400

    Telephone Validator finished

--- script.js
+++ script.js
@@ -1119,14 +1119,9 @@
 // console.log(rot13("SERR PBQR PNZC!"));
 
 //Telephone Number Validator
-//Valid formats included:
-// 555-555-5555
-// (555)555-5555
-// (555) 555-5555
-// 555 555 5555
-// 5555555555
-// 1 555 555 5555
 function telephoneCheck(str) {
-  
+  let regex =  /^(1[\-\s]?)?(\(\d{3}\)|\d{3})[\-\s]?\d{3}[\-\s]?\d{4}$/;
+  //return str.match(regex);
+  return regex.test(str);
 }
-console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("15555555555"));
