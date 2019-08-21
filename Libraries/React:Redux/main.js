7372cd4dded3af75dab86734f22df691ebc6e939
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-21 13:35:45 -0400

    React/Redux practice setup

---
+++ Libraries/React:Redux/main.js
@@ -0,0 +1,13 @@
+ReactDOM.render(JSX, document.getElementById("root"));
+
+const JSX = <div>
+        {/*HTML elements need to be nested within one parent element*/}
+        <h1>Three Things</h1>
+        <p>This is a list of three things.</p>
+        <ul>
+            <li>Thing1</li>
+            <li>Thing2</li>
+            <li>Thing3</li>
+        </ul>
+    </div>
+
